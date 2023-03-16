3c44404937b7170c45a28b4668ac1c387c1c6eb0
n3rdium <leenagajbhiye1003@gmail.com>
2023-03-16 20:24:41 +0530

    Some updates

--- main.py
+++ main.py
@@ -21,19 +21,13 @@ class AstroPi(QtWidgets.QMainWindow):
         # Get the current width and height of the window
         width = size.width()
         height = size.height()
-        
         new_width = width - 330
+        
         self.label_17.resize(new_width, height - 90)
         self.tabWidget.resize(330, height)
         self.tabWidget.move(new_width, 0)
         self.scrollArea_4.resize(new_width, 90)
         self.scrollArea_4.move(0, height - 140)
-        self.scrollArea.resize(new_width, height - 128)
-        self.scrollArea_2.resize(new_width, height - 128)
-        self.scrollArea_3.resize(new_width, height - 128)
-        self.label_4.move(self.label_4.x(), height - 10)
-        self.label_14.move(self.label_14.y(), height - 10)
-        self.label_16.move(self.label_16.x(), height - 10)
 
 if __name__ == "__main__":
     # Create an instance of QtWidgets.QApplication
