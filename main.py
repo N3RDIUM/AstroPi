f1109cc846cfbbd619ca7583c010dc47f7027a9b
n3rdium <leenagajbhiye1003@gmail.com>
2023-03-16 23:01:53 +0530

    Added nice logging features!

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # imports
 from PyQt5 import QtWidgets, uic
+import logging
+import time
 import sys
+
+logging.basicConfig(filename="log.txt", level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s")
 
 class AstroPi(QtWidgets.QMainWindow):
     """
@@ -12,6 +16,11 @@ class AstroPi(QtWidgets.QMainWindow):
         """
         super(AstroPi, self).__init__() # Call the inherited classes __init__ method
         uic.loadUi('./UI/MainWindow.ui', self) # Load the .ui file
+        
+        # Log success
+        self.log("<strong>AstroPi v2023.0.1</strong>")
+        self.log("AstroPi has started successfully!")
+        
         self.show() # Show the GUI
         
         # Set the background image of the window as the AstroPi logo
@@ -36,20 +45,36 @@ class AstroPi(QtWidgets.QMainWindow):
         self.label_17.resize(new_width, height - 160)
         self.tabWidget.resize(330, height)
         self.tabWidget.move(new_width, 0)
-        self.textEdit.resize(new_width, 160)
+        self.textEdit.resize(new_width, 90)
         self.textEdit.move(0, height - 150)
         self.toolButton.move(new_width - 32, height - 150)
         self.toolButton.resize(32, 32)
         self.toolButton_2.move(new_width - 32, height - 118)
         self.toolButton_2.resize(32, 32)
         
-        self.log("Window resized to: " + str(width) + "x" + str(height))
+        self.log("Window resized to: " + str(width) + "x" + str(height), logging.DEBUG)
         
-    def log(self, text):
+    def log(self, text, level=logging.INFO):
         """
         Log text to the textEdit
         """
-        self.textEdit.append(text)
+        # Get timestamp
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        if level == logging.INFO:
+            self.textEdit.append(f"<font color=\"green\">{timestamp} [INFO] " + text + "</font>")
+            logging.info(text)
+        elif level == logging.WARNING:
+            self.textEdit.append(f"<font color=\"yellow\">{timestamp} [WARNING] " + text + "</font>")
+            logging.warning(text)
+        elif level == logging.ERROR:
+            self.textEdit.append(f"<font color=\"red\">{timestamp} [ERROR] " + text + "</font>")
+            logging.error(text)
+        elif level == logging.DEBUG:
+            self.textEdit.append(f"<font color=\"white\">{timestamp} [DEBUG] " + text + "</font>")
+            logging.debug(text)
+        else:
+            self.textEdit.append(f"<font color=\"blue\">{timestamp} [UNKNOWN] " + text + "</font>")
+            
         
     def clearLog(self):
         """
@@ -61,22 +86,28 @@ class AstroPi(QtWidgets.QMainWindow):
         """
         Save the textEdit to a file
         """
-        # Create a file dialog
-        fileDialog = QtWidgets.QFileDialog()
-        fileDialog.setAcceptMode(QtWidgets.QFileDialog.AcceptSave)
-        
-        # Set the file dialog to only accept .txt files
-        fileDialog.setNameFilter("Text files (*.txt)")
-        # Set the default file name
-        fileDialog.selectFile("log.txt")
-        
-        # Show the file dialog
-        fileDialog.exec_()
-        
-        # Get the selected file name
-        fileName = fileDialog.selectedFiles()[0]
-        with open(fileName, "w") as f:
-            f.write(self.textEdit.toPlainText())
+        try:
+            # Create a file dialog
+            fileDialog = QtWidgets.QFileDialog()
+            fileDialog.setAcceptMode(QtWidgets.QFileDialog.AcceptSave)
+            
+            # Set the file dialog to only accept .txt files
+            fileDialog.setNameFilter("Text files (*.txt)")
+            # Set the default file name
+            fileDialog.selectFile("log.txt")
+            
+            # Show the file dialog
+            fileDialog.exec_()
+            
+            # Get the selected file name
+            fileName = fileDialog.selectedFiles()[0]
+            with open(fileName, "w") as f:
+                f.write(self.textEdit.toPlainText())
+        except IndexError:
+            # No file was selected
+            self.log("No file was selected", logging.WARNING)
+        except Exception as e:
+            self.log("Error saving log: " + str(e), logging.ERROR)
 
 if __name__ == "__main__":
     # Create an instance of QtWidgets.QApplication
