25d6652d48afb5c150e70379137ec9e3ab4184f9
n3rdium <leenagajbhiye1003@gmail.com>
2023-03-24 11:08:15 +0530

    Bugfix: Stream 'use_case' is not defined

--- Pi/camera_test.py
+++ Pi/camera_test.py
@@ -8,9 +8,6 @@ capture_config = picam2.create_still_configuration(
     raw={
         "size": (4608, 2592),    
     },
-    main={
-        "size": (4608, 2592),    
-    },
     controls={
         'ExposureTime': 12000000,
         'LensPosition': 0.0,
@@ -26,8 +23,7 @@ time.sleep(2)
 print("[PiCamera2] Starting capture...")
 captured = 0
 while True:
-    buffers, metadata = picam2.capture_buffers(capture_config, ["main", "raw"])
-    picam2.helpers.save(picam2.helpers.make_image(buffers[0], capture_config["main"]), metadata, f"{captured}.jpg")
-    picam2.helpers.save_dng(buffers[1], metadata, capture_config["raw"], f"{captured}.dng")
+    # Capture DNG image
+    picam2.capture_file(capture_config, f"{captured}.dng", name="raw")
     captured += 1
     print(f"[PiCamera2] Captured RAW {captured}.dng")
