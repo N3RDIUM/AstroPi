2cda48043091e9700ea68e817c574cb69d04149e
n3rdium <leenagajbhiye1003@gmail.com>
2023-05-16 16:45:14 +0530

    ...

---
+++ boardcon.py
@@ -0,0 +1,4 @@
+import time
+import socket
+import threading
+
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 # Imports
-from PyQt5 import QtWidgets, uic, QtCore
+from PyQt5 import QtWidgets, uic, QtGui, QtCore
+
+# Constants
+COLORS = {
+    "log": "white",
+    "info": "blue",
+    "warning": "yellow",
+    "error": "red",
+    "critical": "red"
+}
 
 class AstroPi(QtWidgets.QMainWindow):
     """
@@ -11,6 +20,55 @@ class AstroPi(QtWidgets.QMainWindow):
         """
         super(AstroPi, self).__init__() # Call the inherited classes __init__ method
         uic.loadUi('./UI/MainWindow.ui', self) # Load the .ui file
+        self.setupUi() # Setup the GUI
+        
+    def setupUi(self):
+        """
+        Setup the GUI
+        """
+        # Set the Style Sheets
+        self.setStyleSheet("background-color: #222222; color: white;")
+        
+        self.Preview.currentWidget().setStyleSheet("""background-image: url(\"./assets/AstroPi.png\"); 
+                                    background-repeat: no-repeat; 
+                                    background-position: center; 
+                                    background-color: black;""")
+        
+        self.log("""<pre><font color="turquoise">
+  /$$$$$$              /$$                         /$$$$$$$  /$$
+ /$$__  $$            | $$                        | $$__  $$|__/
+| $$  \ $$  /$$$$$$$ /$$$$$$    /$$$$$$   /$$$$$$ | $$  \ $$ /$$
+| $$$$$$$$ /$$_____/|_  $$_/   /$$__  $$ /$$__  $$| $$$$$$$/| $$
+| $$__  $$|  $$$$$$   | $$    | $$  \__/| $$  \ $$| $$____/ | $$
+| $$  | $$ \____  $$  | $$ /$$| $$      | $$  | $$| $$      | $$
+| $$  | $$ /$$$$$$$/  |  $$$$/| $$      |  $$$$$$/| $$      | $$
+|__/  |__/|_______/    \___/  |__/       \______/ |__/      |__/</font></pre><br>
+<h2><font color="turquoise">AstroPi v0.1-alpha</font></h2><br>
+Welcome to AstroPi v0.1-alpha! This is a very early version of the software, so expect bugs and missing features.""", "log")
+        
+        self.textEdits = [
+            self.BoardIP, 
+            self.ResolutionX, 
+            self.ResolutionY, 
+            self.ImageCount, 
+            self.Interval, 
+            self.ExposureTime
+        ]
+        for textEdit in self.textEdits:
+            textEdit.setStyleSheet("background-color: #333333; color: white;")
+        
+        self.SettingsReview.setStyleSheet("background-color: #333333; color: white;")
+        
+        
+        
+    def log(self, message, level):
+        """
+        Log a message to the log window
+        """
+        color = COLORS[level]
+        if level == "critical":
+            message = "<b>" + message + "</b>"
+        self.Console.append("<font color='" + color + "'>" + message + "</font>&nbsp;")
         
 # Run the program
 if __name__ == '__main__':
