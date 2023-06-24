4e6c690fea084c561e1a97fb9bd38a3b96e79c26
N3RDIUM <leenagajbhiye1003@gmail.com>
2023-06-24 14:44:58 +0530

    ..

--- pimain.py
+++ pimain.py
@@ -80,28 +80,12 @@ class FileTransferThread:
                 data = base64.b64encode(f.read()).decode("utf-8")
             log("[IMAGE_TRANSFER ASTROPI] Sending file: " + path, "debug", self.conn)
             print("\r[IMAGE_TRANSFER ASTROPI] Sending file: " + path)
-            for dat in self.split_str_to_batches(data, 8192):
-                self.conn.send(dat)
+            for i in range(0, len(data), 4096):
+                self.conn.send(data[i:i+4096].encode("utf-8"))
+                print(f"\rSent {i+4096}/{len(data)} bytes")
+            time.sleep(0.1)
             self.conn.send("|||".encode("utf-8"))
             os.remove(path)
-            
-    @staticmethod
-    def split_str_to_batches(string, size):
-        idx = 0
-        stop = False
-        ret = []
-        while not stop:
-            if len(string) < size:
-                ret = [string]
-                stop = True
-            else:
-                if idx+size > len(string):
-                    ret.append(string[idx:])
-                else:
-                    ret.append(string[idx:idx+size])
-                    idx += size
-                    if idx >= len(string):
-                        stop = True            
 
 log("Listening for connections...")
 conn, addr = _socket.accept()
