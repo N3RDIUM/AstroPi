cbd9eedaeccc9397517da31f1cd0fe02354c36c7
n3rdium <n3rdium@gmail.com>
2024-04-01 14:53:24 +0530

    Updating stuff

--- camera.py
+++ camera.py
@@ -30,11 +30,11 @@ class Camera:
         self.camera.stop()
         
     def reload_config(self):
-        self.preview_config = picamera2.create_preview_configuration(main={}, controls={
+        self.preview_config = picamera2.CameraConfiguration(main={}, controls={
             "ExposureTime": self.settings['exposure'], 
             "AnalogueGain": self.settings['iso'] * 100
         })
-        self.capture_config = picamera2.create_still_configuration(raw={}, controls={
+        self.capture_config = picamera2.CameraConfiguration(raw={}, controls={
             "ExposureTime": self.settings['exposure'], 
             "AnalogueGain": self.settings['iso'] * 100
         })
