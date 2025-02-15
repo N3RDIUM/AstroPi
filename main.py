77599d7e3aa6ee1cb5108e12c060ecc747fb4b0b
n3rdium <n3rdium@gmail.com>
2025-02-15 08:59:51 +0530

    Boilerplate

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
+import sys
+import zmq
+import logging
+from flask import Flask
+
+# Init logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.DEBUG)
+stdout_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler('astropi.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stdout_handler)
+
+# Init 0mq
+logger.log(logging.DEBUG, "[main] Initializing 0mq")
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+_ = socket.bind("tcp://*:457121")
+
+# Init flask
+logger.log(logging.DEBUG, "[main] Initializing Flask")
+app = Flask(__name__)
+flasklog = logging.getLogger('werkzeug')
+flasklog.disabled = True
+
+# Flask routes
+@app.route("/")
+def root():
+    return "AstroPi Rewrite"
+
+# Driver
+if __name__ == "__main__":
+    logger.log(logging.INFO, "[main] Running flask server on host 0.0.0.0 port 457120")
+    app.run(host="0.0.0.0", port=457120, debug=False)
 
