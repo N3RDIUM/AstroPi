4a7861c26bbde575e60ed35bddba4ec7b4aec8cf
n3rdium <n3rdium@gmail.com>
2026-03-22 21:52:09 +0530

    feat: boilerplate api code

---
+++ server/camera.py
@@ -0,0 +1,177 @@
+import os
+from uuid import uuid4
+from dataclasses import dataclass
+from threading import Thread
+import picamera2
+from picamera2.platform import Enum
+import time
+
+CAPTURE_DIR = "./capture"
+if not os.path.isdir(CAPTURE_DIR):
+    os.mkdir(CAPTURE_DIR)
+
+type Microseconds = int
+
+@dataclass
+class CameraControls:
+    AnalogueGain: float
+    ExposureTime: Microseconds
+
+@dataclass
+class IntervalometerControls:
+    Count: int
+    Cooldown: Microseconds
+
+@dataclass
+class Command: ...
+
+@dataclass
+class UpdateControls:
+    controls: CameraControls
+
+@dataclass
+class UpdateIntervalomater:
+    controls: IntervalometerControls
+
+@dataclass
+class StartPreview: ...
+
+@dataclass
+class StopPreview: ...
+
+@dataclass
+class StartCapture: ...
+
+@dataclass
+class StopCapture: ...
+
+class DeviceState(Enum):
+    STOPPED = 0
+    PREVIEW = 1
+    SHOOTING = 2
+
+@dataclass
+class Intervalometer:
+    state: DeviceState
+    controls: IntervalometerControls
+
+
+class Camera:
+    """Wraps Picamera2 for use in a flask server"""
+
+    command_queue: list[Command]
+    device_thread: Thread
+    alive: bool
+
+    def __init__(self) -> None:
+        self.command_queue = []  # FIFO
+        self.alive = True
+
+        self.device_thread = Thread(
+            target = self._device_thread,
+            daemon = True
+        )
+        self.device_thread.start()
+
+    def _device_thread(self):
+        device = picamera2.Picamera2()
+        intervalometer = Intervalometer(
+            controls = IntervalometerControls(
+                Count = 0,
+                Cooldown = 0
+            ),
+            state = DeviceState.STOPPED
+        )
+
+        while self.alive:
+            if intervalometer.state == DeviceState.PREVIEW:
+                continue
+            elif intervalometer.state == DeviceState.SHOOTING:
+                device.start()
+                filename = os.path.join(CAPTURE_DIR, f"{uuid4()}.dng")
+                _ = device.capture_file(filename)
+                device.stop()
+
+                # TODO call/provide hooks
+                
+                time.sleep(intervalometer.controls.Cooldown)
+                intervalometer.controls.Count -= 1
+                continue
+
+            if len(self.command_queue) != 0:
+                cmd = self.command_queue.pop(0)
+                cmd_type = type(cmd)
+
+                if cmd_type == Command:
+                    print("[WARN] device received empty command")
+                    continue
+
+                elif isinstance(cmd, UpdateControls):
+                    if intervalometer.state != DeviceState.STOPPED:
+                        print("[ERR] device not stopped, cannot update controls")
+                        continue
+
+                    try:
+                        controls = cmd.controls
+                        device.set_controls({
+                            "AnalogueGain": controls.AnalogueGain,
+                            "ExposureTime": controls.ExposureTime
+                        })
+                        print("[INFO] set device controls successfully")
+                    except Exception as e:
+                        print(f"[ERR] could not set device controls: {e}")
+                        continue
+
+                elif isinstance(cmd, UpdateIntervalomater):
+                    if intervalometer.state != DeviceState.STOPPED:
+                        print("[ERR] device not stopped, cannot update intervalometer")
+                        continue
+
+                    intervalometer.controls = cmd.controls
+
+                elif isinstance(cmd, StartPreview):
+                    if intervalometer.state == DeviceState.PREVIEW:
+                        print("[INFO] preview already running!")
+                    elif intervalometer.state == DeviceState.SHOOTING:
+                        print("[ERR] cannot start preview mode while shooting")
+                        continue
+
+                    intervalometer.state = DeviceState.PREVIEW
+
+                elif isinstance(cmd, StopPreview):
+                    if intervalometer.state == DeviceState.STOPPED:
+                        print("[INFO] preview already stopped!")
+                    elif intervalometer.state == DeviceState.SHOOTING:
+                        print("[ERR] cannot stop preview mode while shooting")
+                        continue
+
+                    intervalometer.state = DeviceState.STOPPED
+
+                elif isinstance(cmd, StartCapture):
+                    if intervalometer.state == DeviceState.SHOOTING:
+                        print("[INFO] already shooting!")
+                    elif intervalometer.state == DeviceState.PREVIEW:
+                        print("[ERR] cannot start shooting in preview mode")
+                        continue
+
+                    intervalometer.state = DeviceState.SHOOTING
+
+                elif isinstance(cmd, StopCapture):
+                    if intervalometer.state == DeviceState.STOPPED:
+                        print("[INFO] already stopped!")
+                    elif intervalometer.state == DeviceState.PREVIEW:
+                        print("[ERR] cannot stop shooting in preview mode")
+                        continue
+
+                    intervalometer.state = DeviceState.STOPPED
+
+                else:
+                    print(f"[ERR] unknown command type: {cmd_type}")
+
+    def submit_command(self, command: Command) -> None:
+        self.command_queue.append(command)
+
+    def kill(self) -> None:
+        self.alive = False
+        self.device_thread.join()
+
--- server/main.py
+++ server/main.py
@@ -1,10 +1,133 @@
-from flask import Flask
+import atexit
+
+from flask import Flask, request, jsonify
+from .camera import (
+    Camera,
+    CameraControls,
+    IntervalometerControls,
+    UpdateControls,
+    UpdateIntervalomater,
+    StartPreview,
+    StartCapture,
+    StopPreview,
+    StopCapture,
+    Microseconds
+)
+
+
+DEFAULT_EXPOSURE_TIME: Microseconds = 100000
+DEFAULT_ANALOGUE_GAIN: float = 8.0
+DEFAULT_COUNT: int = 1
+DEFAULT_COOLDOWN: int = 100000
 
 app = Flask(__name__)
+cam = Camera()
+_ = atexit.register(cam.kill)
 
 @app.route('/')
 def home():
-    return 'hello world from astropi'
+    return jsonify({
+        "response": "hello world from astropi"
+    }), 200
+
+@app.route('/camera_controls/', methods=["POST"])
+def cam_controls():
+    controls = request.form
+
+    configuration = CameraControls(
+        ExposureTime = DEFAULT_EXPOSURE_TIME,
+        AnalogueGain = DEFAULT_ANALOGUE_GAIN
+    )
+
+    if "exposure" in controls:
+        try:
+            configuration.ExposureTime = int(controls["exposure"])
+        except Exception as e:
+            return jsonify({
+                "response": f"failed to set exposure: {e}"
+            }), 500
+
+    if "gain" in controls:
+        try:
+            configuration.AnalogueGain = float(controls["gain"])
+        except Exception as e:
+            return jsonify({
+                "response": f"failed to set exposure: {e}"
+            }), 500
+
+    cam.submit_command(UpdateControls(
+        controls = configuration
+    ))
+
+    return jsonify({
+        "response": "success"
+    }), 200
+
+@app.route('/intervalometer_controls/', methods=["POST"])
+def intervalometer_controls():
+    controls = request.form
+
+    configuration = IntervalometerControls(
+        Count = DEFAULT_COUNT,
+        Cooldown = DEFAULT_COOLDOWN
+    )
+
+    if "count" in controls:
+        try:
+            configuration.Count = int(controls["count"])
+        except Exception as e:
+            return jsonify({
+                "response": f"failed to set count: {e}"
+            }), 500
+
+    if "cooldown" in controls:
+        try:
+            configuration.Cooldown = int(controls["cooldown"])
+        except Exception as e:
+            return jsonify({
+                "response": f"failed to set cooldown: {e}"
+            }), 500
+
+    cam.submit_command(UpdateIntervalomater(
+        controls = configuration
+    ))
+
+    return jsonify({
+        "response": "success"
+    }), 200
+
+@app.route('/start-preview/')
+def start_preview():
+    cam.submit_command(StartPreview())
+
+    return jsonify({
+        "response": "success"
+    }), 200
+
+@app.route('/stop-preview/')
+def stop_preview():
+    cam.submit_command(StopPreview())
+
+    return jsonify({
+        "response": "success"
+    }), 200
+
+@app.route('/start-capture/')
+def start_capture():
+    cam.submit_command(StartCapture())
+
+    return jsonify({
+        "response": "success"
+    }), 200
+
+@app.route('/stop-capture/')
+def stop_capture():
+    cam.submit_command(StopCapture())
+
+    return jsonify({
+        "response": "success"
+    }), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
